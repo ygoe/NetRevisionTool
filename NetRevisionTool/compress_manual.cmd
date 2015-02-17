1559885d4122bbb621b9cf7d8dea3004a2eb94d5
Yves Goergen <yves@unclassified.de>
2015-02-17 17:48:56 +0100

    Use included UnxUtils gzip to compress manual instead of installed 7-Zip
    This tool is included in the repository, making the process more portable. Also, the compression result is always the same, no matter which 7-Zip version is installed. Currently, the compressed file is 73 bytes longer than with 7-Zip which is negligible.

--- NetRevisionTool/compress_manual.cmd
+++ NetRevisionTool/compress_manual.cmd
@@ -1,7 +1,5 @@
 @echo off
 cd /d "%~dp0"
 echo Compressing Manual.txt file...
-if exist Manual.txt.gz del Manual.txt.gz
-rem %ProgramFiles% is "C:\Program Files (x86)" in Visual Studio :(
-"C:\Program Files\7-Zip\7z.exe" a Manual.txt.gz Manual.txt -mx=9 || exit 1
+gzip -c9 Manual.txt >Manual.txt.gz || exit 1
 exit 0
